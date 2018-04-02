484f2e32219eb91d3832d0f0cf8dd4c59ce688c0
Darnell Willis <darnellwillis96@gmail.com>
2018-04-02 15:44:01 -0700

    Update mysqlconnection.py

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -8,7 +8,7 @@ class MySQLConnection(object):
                 'host': 'localhost',
                 'database': db, # we got db as an argument
                 'user': 'root',
-                'password': 'Applejuice18',
+                'password': 'root',
                 'port': '3306' # change the port to match the port your SQL server is running on
         }
         # this will use the above values to generate the path to connect to your sql database
